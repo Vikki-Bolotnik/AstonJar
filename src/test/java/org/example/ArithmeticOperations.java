8a40500526daa7ffb312d991e59d423f841d5b30
Viktoria Bolotnik <vikabolotnik@gmail.com>
2025-06-03 20:08:28 +0300

    refactoring

---
+++ src/test/java/org/example/ArithmeticOperations.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ArithmeticOperationsTest {
+
+  private final ArithmeticOperations operations = new ArithmeticOperations();
+
+  @Test
+  public void testAdd() {
+    Assert.assertEquals(operations.add(5, 3), 8);
+    Assert.assertEquals(operations.add(-2, 2), 0);
+  }
+
+  @Test
+  public void testSubtract() {
+    Assert.assertEquals(operations.subtract(10, 4), 6);
+    Assert.assertEquals(operations.subtract(4, 10), -6);
+  }
+
+  @Test
+  public void testMultiply() {
+    Assert.assertEquals(operations.multiply(6, 7), 42);
+    Assert.assertEquals(operations.multiply(-3, 4), -12);
+  }
+
+  @Test
+  public void testDivide() {
+    Assert.assertEquals(operations.divide(10, 2), 5.0);
+    Assert.assertEquals(operations.divide(-9, 3), -3.0);
+  }
+
+  @Test(expectedExceptions = ArithmeticException.class)
+  public void testDivideByZero() {
+    operations.divide(5, 0);
+  }
+}
---
+++ src/test/java/org/example/TriangleAreaCalculator.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TriangleAreaCalculatorTest {
+
+  private final TriangleAreaCalculator calculator = new TriangleAreaCalculator();
+
+  @DataProvider(name = "areaData")
+  public Object[][] createData() {
+    return new Object[][]{
+        {10.0, 5.0, 25.0},
+        {0.0, 10.0, 0.0},
+        {7.5, 3.0, 11.25}
+    };
+  }
+
+  @Test(dataProvider = "areaData")
+  public void testCalculateArea(double base, double height, double expected) {
+    Assert.assertEquals(calculator.calculateArea(base, height), expected, 1e-9);
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testCalculateAreaNegativeBase() {
+    calculator.calculateArea(-5, 10);
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testCalculateAreaNegativeHeight() {
+    calculator.calculateArea(5, -10);
+  }
+}
