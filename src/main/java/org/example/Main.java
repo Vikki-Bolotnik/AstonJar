6c742b9f2a650dad77dda32dfe7748bdbfa1bc44
Viktoria Bolotnik <vikabolotnik@gmail.com>
2025-06-21 16:21:17 +0300

    refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,5 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
 
     public static void main(String[] args) {
-        ChromeDriver driver = new ChromeDriver();
-        try {
-            driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java/4.33.0");
-        } finally {
-            driver.quit();
-        }
     }
 }
